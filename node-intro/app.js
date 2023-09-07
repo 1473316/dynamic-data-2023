1cc3cc466349bf5f402d927f22a85c1f1dc95348
1473316 <dnk37@miami.edu>
2023-09-07 19:50:49 -0400

    update

---
+++ node-intro/app.js
@@ -0,0 +1,36 @@
+var name = "Dominique"
+var myNumber = 22
+const port = 300
+
+var car= {
+    make:"Audi",
+    model:"55",
+    year: "2023",
+}
+
+function outputsomething (output){
+    console.log("you wrote " + output);
+}
+
+outputsomething("Write something here...i'm having fun with node");
+
+var result = addTwo(10);
+
+//outputsomething(result);
+
+//outputsomething(addTwo(10));
+
+//console.log("Hello Class!");
+
+
+//console.log(car);
+
+//access object properties with "."
+
+//outputsomething(car.model);
+
+//console.log("My name is " + name + "and my fa")
+
+console.log(name + myNumber);
+console.log(myNumber +myNumber);
+console.log(myNumber + "22");
---
+++ node-intro/routeshelloworld.js
@@ -0,0 +1,33 @@
+//Setup our server
+const http = require ('http');
+//Define the port the app will be accessed from (80,8080,8888 are default to the domain/)
+const port = process.env.PORT || 4000;
+
+const server = http.createServer((request,response) => {
+//Get the url
+const path = request.url;
+
+if((path == "") || (path == "/")) {
+    response.writeHead(200 , { "Content-Type" : "Text/Plain "})
+    response.end ("Home Page")
+} else if (path == "/about") {
+    response.writeHead(200 , { "Content-Type" : "Text/Plain "})
+    response.end ("About Page")
+} else if (path == "/contanct") {
+    response.writeHead(200 , { "Content-Type" : "Text/Plain "})
+    response.end ("<h1>Contact Us</h1>")
+} else {
+    response.writeHead(404 , { "Content-Type" : "Text/Plain "})
+    response.end ("Not Found")
+} 
+
+
+
+    //response.writeHead(200 , { "Content-Type" : "Text/Plain "})
+   // response.end ("Hello World")
+}) 
+
+//start the server
+server.listen(port, () => console.log("server started on port " + port + " press ctrl + c to stop"))
+
+
