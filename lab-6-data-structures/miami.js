d89e8e882181aa815f8517c3e3db0df1f7004f3d
1473316 <dnk37@miami.edu>
2023-10-08 12:08:24 -0400

    push

---
+++ lab-6-data-structures/miami.js
@@ -0,0 +1,62 @@
+const express = require('express')
+
+//add the view engine
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+//configure our express app to use handlebars
+app.engine('handlebars',expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine','handlebars')
+//ends handlebar configuration
+
+//Static files or folders are speicified before any route
+app.use(express.static(__dirname + "/public"))
+
+const port = process.env.port || 3000;
+//Routes go before 404 and 500
+
+app.get('/',(req,res)=>{
+    var data = require('./data/home-data.json')
+    res.render('page',{data})
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about', {
+        title:"About Miami",
+        pageTitle:"About Miami Travel",
+        image:"1.jpg",
+        description:"Miami is a beautiful city"
+    })
+})
+//This generates an error because the parameter names don't match
+//request should be res
+app.get('/nightlife',(req,res)=>{
+   res.render('nightlife') 
+})
+
+//Error handling --> app.use() basic express route 
+app.use((req,res) => {
+    res.status(404)
+    res.render('404')
+})
+
+//Server Error 500
+app.use((error,req,res,next) => {
+    console.log(error.message)
+    res.status(500)
+    res.render('500')
+})
+ //setup listener
+app.listen(port,()=>{
+    //console.log('Server started http://localhost:${port}')
+    console.log('Server starter http://localhost:'+port)
+    console.log('To close press Ctrl-C')
+
+})
+
+
+
